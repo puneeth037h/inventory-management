865583e8545e63427708b616d765436be7b7d982
puneeth037h <punithshanakanahalli@gmail.com>
2024-03-01 21:18:21 +0530

    delete seller

--- src/components/Seller/Seller.js
+++ src/components/Seller/Seller.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Seller(){
     const [sellersdata, setsellersData] = useState([]);
-
+    let [result, setresult] = useState('')
     useEffect(() => {
         fetch('http://localhost:3000/seller')
             .then(res => res.json())
@@ -16,7 +16,21 @@ function Seller(){
             });
             console.log(sellersdata);
     }, []);
-
+    function del(sellerIdToDelete) {
+        fetch('http://localhost:3000/deleteseller', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ sellerId: sellerIdToDelete })
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            setresult(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+        window.location.reload();
+    }
     return (
         <div>
             <div>
@@ -31,7 +45,7 @@ function Seller(){
                         <p>{elem.phone}</p>
                         <p>{elem.address}</p>
                         <Link to={`/updateseller/${elem.sellerId}`}><button>edit</button></Link>
-                        <Link><button>delete</button></Link>
+                        <button onClick={() => del(elem.sellerId)}>delete</button>
                     </div>
                 );
             })}
